959859ef7a0e7bb50f0ea494694e4d013e2b88a5
Nitu Kumari <nituspj032001@gmail.com>
2026-09-07 00:37:11 +0530

    fix: implement JwtAuthFilter to process tokens and add getUserById to UserService for authentication support

--- src/main/java/com/SecurityApp/services/PostServiceImpl.java
+++ src/main/java/com/SecurityApp/services/PostServiceImpl.java
@@ -2,17 +2,21 @@ package com.SecurityApp.services;
 
 import com.SecurityApp.dto.PostDto;
 import com.SecurityApp.entities.PostEntity;
+import com.SecurityApp.entities.User;
 import com.SecurityApp.exceptions.ResourceNotFoundException;
 import com.SecurityApp.respositories.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
+
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
@@ -36,6 +40,8 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto getPostById(Long postId) {
+        User user= (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("user {}",user);
         PostEntity postEntity = postRepository.findById(postId)
                 .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
         return modelMapper.map(postEntity, PostDto.class);
