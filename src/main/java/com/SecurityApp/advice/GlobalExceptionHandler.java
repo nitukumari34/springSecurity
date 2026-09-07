ef5c05d83c00e0b3dee7c117e7ebddf4ad60446f
Nitu Kumari <nituspj032001@gmail.com>
2026-09-07 23:04:19 +0530

    feat: implement global exception handling in JwtAuthFilter using HandlerExceptionResolver

--- src/main/java/com/SecurityApp/advice/GlobalExceptionHandler.java
+++ src/main/java/com/SecurityApp/advice/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.SecurityApp.advice;
 
 import com.SecurityApp.exceptions.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.http.HttpStatus;
+
+import org.springframework.security.core.AuthenticationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,6 +16,17 @@ public class GlobalExceptionHandler {
         ApiError apiError=new ApiError(exception.getLocalizedMessage(), HttpStatus.NOT_FOUND);
         return  new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
 
+    }
+    @ExceptionHandler(AuthenticationException.class)
+    public  ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException ex){
+      ApiError apiError= new ApiError(ex.getLocalizedMessage(),HttpStatus.UNAUTHORIZED);
+      return new  ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public  ResponseEntity<ApiError> handleJwtException(JwtException ex){
+        ApiError apiError= new ApiError(ex.getLocalizedMessage(),HttpStatus.UNAUTHORIZED);
+        return new  ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
     }
 }
 
