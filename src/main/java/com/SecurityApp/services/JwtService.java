d4564d74d3d4a505f420dc42a3abcc4fd94b8540
Nitu Kumari <nituspj032001@gmail.com>
2026-09-08 21:49:14 +0530

    feat: implement JWT access and refresh token strategy with environment configuration

--- src/main/java/com/SecurityApp/services/JwtService.java
+++ src/main/java/com/SecurityApp/services/JwtService.java
@@ -19,13 +19,36 @@ public class JwtService {
     private SecretKey getSecretKey(){
         return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
     }
-    public  String generateToken(User user){
-       return Jwts.builder()
+    //only one token generate
+//    public  String generateToken(User user){
+//       return Jwts.builder()
+//                .subject(user.getId().toString())
+//               .claim("email",user.getEmail()).
+//               claim("roles", Set.of("ADMIN","USER"))
+//                .issuedAt(new Date())
+//                .expiration(new Date(System.currentTimeMillis() + 1000 * 60)) // 1 hour
+//                .signWith(getSecretKey())
+//                .compact();
+//
+//    }
+    //now generate two token // short lived
+public  String generateAccessToken(User user){
+    return Jwts.builder()
+            .subject(user.getId().toString())
+            .claim("email",user.getEmail()).
+            claim("roles", Set.of("ADMIN","USER"))
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + 1000 * 60*10))    
+            .signWith(getSecretKey())
+            .compact();
+
+}
+//long lived production  ready feature
+    public  String generateRefreshToken(User user){
+        return Jwts.builder()
                 .subject(user.getId().toString())
-               .claim("email",user.getEmail()).
-               claim("roles", Set.of("ADMIN","USER"))
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60)) // 1 hour
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60*60*24*30*6))
                 .signWith(getSecretKey())
                 .compact();
 
