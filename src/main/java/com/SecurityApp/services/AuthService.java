18dfe4643cbab3e7e22c3b78ba66d54e3647e898
Nitu Kumari <nituspj032001@gmail.com>
2026-09-08 21:49:08 +0530

    feat: implement dual-token authentication system with access and refresh tokens

--- src/main/java/com/SecurityApp/services/AuthService.java
+++ src/main/java/com/SecurityApp/services/AuthService.java
@@ -1,8 +1,10 @@
 package com.SecurityApp.services;
 
 import com.SecurityApp.dto.LoginDTO;
+import com.SecurityApp.dto.LoginResponseDTO;
 import com.SecurityApp.entities.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,11 +15,23 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private  final AuthenticationManager authenticationManager;
     private  final  JwtService jwtService;
-    public String login(LoginDTO loginDTO){
+    private  final  UserService userService;
+    public LoginResponseDTO login(LoginDTO loginDTO){
         Authentication authentication= authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDTO.getEmail(),loginDTO.getPassword())
         );
         User user=(User)authentication.getPrincipal();
-        return jwtService.generateToken(user);
+//        return jwtService.generateToken(user);
+        String accessToken= jwtService.generateAccessToken(user);
+        String refreshToken= jwtService.generateRefreshToken(user);
+      return  new LoginResponseDTO(user.getId(),accessToken,refreshToken);
+    }
+
+    public LoginResponseDTO refreshToken(String refreshToken) {
+      Long userId=jwtService.getUserIdFromToken(refreshToken);
+      User user= userService.getUserById(userId);
+       String accessToken= jwtService.generateAccessToken(user);
+       return  new LoginResponseDTO(user.getId(),accessToken,refreshToken);
+
     }
 }
