2a9650d6f02f83c496668703f3b7ca4db38d68ba
Nitu Kumari <nituspj032001@gmail.com>
2026-09-08 21:49:02 +0530

    feat: implement dual-token authentication system with access and refresh tokens

--- src/main/java/com/SecurityApp/dto/LoginResponseDTO.java
+++ src/main/java/com/SecurityApp/dto/LoginResponseDTO.java
@@ -1,4 +1,15 @@
 package com.SecurityApp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDTO {
+    private  Long userId;
+    private  String accessToken;
+    private  String refreshToken;
+
 }
