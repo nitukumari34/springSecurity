bb7bbbe2a5fc3f9a0afef1567d26c95919066505
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 22:14:32 +0530

    feat(session): add Session entity and SessionRepository

--- src/main/java/com/SecurityApp/entities/Session.java
+++ src/main/java/com/SecurityApp/entities/Session.java
@@ -1,4 +1,29 @@
 package com.SecurityApp.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "session")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String refreshToken;
+
+    @ManyToOne
+    private User user;
+
+    @CreationTimestamp
+    private LocalDateTime lastUsedAt;
 }
--- src/main/java/com/SecurityApp/respositories/SessionRepository.java
+++ src/main/java/com/SecurityApp/respositories/SessionRepository.java
@@ -1,4 +1,16 @@
 package com.SecurityApp.respositories;
 
-public interface SessionRepository {
+import com.SecurityApp.entities.Session;
+import com.SecurityApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session,Long> {
+    List<Session> findByUser(User user);
+
+    Optional<Session> findByRefreshToken(String refreshToken);
 }
