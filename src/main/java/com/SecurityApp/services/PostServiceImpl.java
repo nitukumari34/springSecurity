1c33280490eea6d49257cce64b4f2ee239154b5f
Nitu Kumari <nituspj032001@gmail.com>
2026-09-13 00:08:32 +0530

    refactor(post): remove user principal casting in getPostById to support unauthenticated access

--- src/main/java/com/SecurityApp/services/PostServiceImpl.java
+++ src/main/java/com/SecurityApp/services/PostServiceImpl.java
@@ -40,8 +40,8 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto getPostById(Long postId) {
-        User user= (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        log.info("user {}",user);
+//        User user= (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        log.info("user {}",user);
         PostEntity postEntity = postRepository.findById(postId)
                 .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
         return modelMapper.map(postEntity, PostDto.class);
