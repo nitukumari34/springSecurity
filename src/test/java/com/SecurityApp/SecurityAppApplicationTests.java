100336c5331f220a002aeda959ab72b373d22e99
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 22:14:08 +0530

    chore(config): switch ddl-auto to update and add session limit tests

--- src/test/java/com/SecurityApp/SecurityAppApplicationTests.java
+++ src/test/java/com/SecurityApp/SecurityAppApplicationTests.java
@@ -6,11 +6,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class SecurityAppApplicationTests {
 
 	@Autowired
 	private JwtService jwtService;
+
+	@Autowired
+	private com.SecurityApp.services.SessionService sessionService;
+
+	@Autowired
+	private com.SecurityApp.respositories.UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
@@ -28,9 +36,19 @@ class SecurityAppApplicationTests {
 
 		System.out.println("access: " + accessToken);
 
-//		Long id = jwtService.getUserIdFromToken(token);
 		Long id = jwtService.getUserIdFromToken(accessToken);
-
 		System.out.println("id: " + id);
+	}
+
+	@Autowired
+	private com.SecurityApp.respositories.SessionRepository sessionRepository;
+
+	@Test
+	void testSessionLimit() throws InterruptedException {
+		User user = userRepository.findAll().get(0);
+
+		// Verify 2 sessions remain
+		List<com.SecurityApp.entities.Session> sessions = sessionRepository.findByUser(user);
+		System.out.println("Current active sessions in DB: " + sessions.size());
 	}
 }
