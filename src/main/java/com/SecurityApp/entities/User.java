ecaeffb2ca57f28986d4ed4cf832c6eb7032a1fd
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 23:46:24 +0530

    feat(user): map roles to SimpleGrantedAuthority in getAuthorities

--- src/main/java/com/SecurityApp/entities/User.java
+++ src/main/java/com/SecurityApp/entities/User.java
@@ -4,10 +4,12 @@ import com.SecurityApp.entities.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
@@ -31,9 +33,13 @@ public class User implements UserDetails {
     @ElementCollection(fetch = FetchType.EAGER)
     @Enumerated(EnumType.STRING)
     private  List<Role>roles;
+
+    //every time returring role along with return authority for that user
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles.stream()
+                .map(role->new SimpleGrantedAuthority("ROLE_"+role.name()))
+                .collect(Collectors.toSet());
     }
 
     @Override
