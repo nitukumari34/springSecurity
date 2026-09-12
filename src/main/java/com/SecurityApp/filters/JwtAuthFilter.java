e8f51a95835e2a80668b9d49ea365812ebdf2c72
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 23:46:38 +0530

    feat(auth): set user authorities in security authentication context

--- src/main/java/com/SecurityApp/filters/JwtAuthFilter.java
+++ src/main/java/com/SecurityApp/filters/JwtAuthFilter.java
@@ -42,7 +42,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 User user = userService.getUserById(userId);
                 UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(user, null, null);
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
                 //like ip address,rate limiting yaha add kr skte h
                 authenticationToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
