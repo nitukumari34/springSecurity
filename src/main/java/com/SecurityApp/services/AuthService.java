91c260f238eb1e9e8672e40282a351c58bf22ae0
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 22:13:33 +0530

    feat(auth): integrate session creation and token validation into login flows

--- src/main/java/com/SecurityApp/services/AuthService.java
+++ src/main/java/com/SecurityApp/services/AuthService.java
@@ -16,6 +16,7 @@ public class AuthService {
     private  final AuthenticationManager authenticationManager;
     private  final  JwtService jwtService;
     private  final  UserService userService;
+    private  final  SessionService sessionService;
     public LoginResponseDTO login(LoginDTO loginDTO){
         Authentication authentication= authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDTO.getEmail(),loginDTO.getPassword())
@@ -24,13 +25,24 @@ public class AuthService {
 //        return jwtService.generateToken(user);
         String accessToken= jwtService.generateAccessToken(user);
         String refreshToken= jwtService.generateRefreshToken(user);
+
+        //generate session
+        sessionService.generateNewSession(user,refreshToken);
+
+
       return  new LoginResponseDTO(user.getId(),accessToken,refreshToken);
     }
 
+    //refresh token
     public LoginResponseDTO refreshToken(String refreshToken) {
       Long userId=jwtService.getUserIdFromToken(refreshToken);
+
+      //check refresh token is valid or not
+        sessionService.validateSession(refreshToken);
       User user= userService.getUserById(userId);
        String accessToken= jwtService.generateAccessToken(user);
+
+       //if user try to refresh token to get new Access token
        return  new LoginResponseDTO(user.getId(),accessToken,refreshToken);
 
     }
