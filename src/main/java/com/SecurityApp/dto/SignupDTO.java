d788f65b2b4e50c156c1acaebe79de75df736ef8
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 22:51:12 +0530

    feat(auth): add roles field to SignupDTO

--- src/main/java/com/SecurityApp/dto/SignupDTO.java
+++ src/main/java/com/SecurityApp/dto/SignupDTO.java
@@ -1,6 +1,9 @@
 package com.SecurityApp.dto;
 
+import com.SecurityApp.entities.enums.Role;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class SignupDTO {
@@ -8,4 +11,6 @@ public class SignupDTO {
     private String email;
     private String password;
     private String  name;
+    //not recommended in prod to give role at signup time im doing for test purpose later i will change
+    private List<Role>roles;
 }
