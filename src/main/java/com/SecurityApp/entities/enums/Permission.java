ac8a2e9682fa2f0a15e5da17f4767eb712a475c2
Nitu Kumari <nituspj032001@gmail.com>
2026-09-13 17:29:13 +0530

    Granular :add Permission enum and PermissionMapping utility

--- src/main/java/com/SecurityApp/entities/enums/Permission.java
+++ src/main/java/com/SecurityApp/entities/enums/Permission.java
@@ -1,4 +1,14 @@
 package com.SecurityApp.entities.enums;
 
 public enum Permission {
+    POST_VIEW,
+    POST_CREATE,
+    POST_UPDATE,
+    POST_DELETE,
+
+
+    USER_VIEW,
+    USER_CREATE,
+    USER_UPDATE,
+    USER_DELETE
 }
--- src/main/java/com/SecurityApp/utils/PermissionMapping.java
+++ src/main/java/com/SecurityApp/utils/PermissionMapping.java
@@ -1,4 +1,26 @@
 package com.SecurityApp.utils;
 
-public class PersmissionMapping {
+import com.SecurityApp.entities.enums.Permission;
+import com.SecurityApp.entities.enums.Role;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.SecurityApp.entities.enums.Permission.*;
+import static com.SecurityApp.entities.enums.Role.*;
+
+public class PermissionMapping {
+   private  static  final Map<Role, Set<Permission>>map=Map.of(
+           USER,Set.of(USER_VIEW,POST_VIEW),
+           CREATOR,Set.of(USER_VIEW,POST_VIEW,USER_UPDATE,POST_UPDATE),
+           ADMIN,Set.of(USER_VIEW,POST_VIEW,USER_UPDATE,POST_UPDATE,USER_DELETE,USER_CREATE,POST_DELETE)
+
+
+   );
+   public  static  Set<SimpleGrantedAuthority>getAuthorityForRole(Role role){
+
+        return map.get(role).stream().map(permission -> new SimpleGrantedAuthority(permission.name())).collect(Collectors.toSet());
+   }
 }
